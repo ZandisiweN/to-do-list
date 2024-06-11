b28c45ef6218e2c8244786f2076649e2c694bc89
Zandisiwe Ndhlovu <ndhlovuzandisiwe@gmail.com>
2024-06-11 08:28:29 +0200

    Adding a new task,added fontawesome icon

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 document.querySelector('#button').onclick = function () {
   if (document.querySelector('#text-input').value.length == 0) {
     alert('Please enter a task...')
+  } else {
+    document.querySelector('#tasks').innerHTML
+      = `
+     <div class = "task">
+     <span id = "taskname">
+      ${document.querySelector('#newtask input').value}
+     </span>
+     <button class="delete">
+        <i class="fa-regular fa-trash-can"></i>
+     </button>
+     <div>
+     `
   }
 }
 
